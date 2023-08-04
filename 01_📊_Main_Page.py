6ec3dfa109d6a947a2e14d481ce8511c4d34bc01
Rodrigo Aroni siquette <68368142+siquette@users.noreply.github.com>
2023-08-04 00:34:51 -0300

    Update 01_📊_Main_Page.py

--- 01_📊_Main_Page.py
+++ 01_📊_Main_Page.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
 
 st.markdown(
     """
- A Cury Company é uma empresa de tecnologia que criou um aplicativo
+ A Cury Company é uma empresa ficticia de tecnologia que criou um aplicativo
 que conecta restaurantes, entregadores e pessoas.
 Através desse aplicativo, é possível realizar o pedido de uma refeição, em
 qualquer restaurante cadastrado, e recebê-lo no conforto da sua casa por
@@ -140,7 +140,7 @@ e gera muitos dados sobre entregas, tipos de pedidos, condições
 climáticas, avaliação dos entregadores e etc. Apesar da entrega estar
 crescento, em termos de entregas, o CEO não tem visibilidade completa
 dos KPIs de crescimento da empresa.
-Você foi contratado como um Cientista de Dados para criar soluções de
+Eu foi contratado como um Cientista de Dados para criar soluções de
 dados para entrega, mas antes de treinar algoritmos, a necessidade da
 empresa é ter um os principais KPIs estratégicos organizados em uma
 única ferramenta, para que o CEO possa consultar e conseguir tomar
